57483ae5437c12f4bb55be2141b6c8505baa1171
Rumeysa-sakarya <rumeysakarya.10@gmail.com>
2025-02-18 17:03:14 +0100

    final update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "9fbad2a6f649tea03aedob9540488afd";
   let context =
-    "you are a romantic poem expert and you love to write short poems of about 2 to 8 lines. your mission is to generate a poem in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element. Do not include a title for the poem and the '```' or 'html'.";
+    "you are a romantic poem expert and you love to write short poems of about 2 to 4 lines. your mission is to generate a poem in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element. Do not include a title for the poem and the '```' or 'html'.";
 
   let prompt = `user instructions: Generate a short turkish poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
